500d52e616225b70ceb732011384c41444318398
ollie81 <oneorgone@gmail.com>
2026-07-05 08:06:41 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
+import 'screens/auth_screen.dart';
+import 'screens/home_screen.dart';
+import 'services/api_service.dart';
+
 void main() {
   runApp(const OllieApp());
 }
@@ -17,7 +21,52 @@ class OllieApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFF0D0F1A),
         fontFamily: 'SF Pro Display',
       ),
-      home: const WelcomeScreen(),
+      home: const AuthWrapper(),
     );
+  }
+}
+
+class AuthWrapper extends StatefulWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  State<AuthWrapper> createState() => _AuthWrapperState();
+}
+
+class _AuthWrapperState extends State<AuthWrapper> {
+  final ApiService _api = ApiService();
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    final isLoggedIn = await _api.isLoggedIn();
+    if (mounted) {
+      setState(() {
+        _isLoading = false;
+      });
+      if (isLoggedIn) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const HomeScreen()),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    return const WelcomeScreen();
   }
 }
