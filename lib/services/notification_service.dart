ca661b5310b6f344e6732449dd5712ec5c893dd2
ollie81 <oneorgone@gmail.com>
2026-07-07 07:01:37 -0700

    Create notification_service.dart

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationService {
+  static Future<void> init() async {}
+  static Future<void> setupFirebase() async {}
+  static Future<String?> getFCMToken() async {
+    return await FirebaseMessaging.instance.getToken();
+  }
+}
